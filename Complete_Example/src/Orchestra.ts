693f8b24fa83fda610253702180bd170bd385499
Ruben Mertens <ruben.mertens@infosupport.com>
2023-04-21 08:15:05 +0200

    made play and stop work again

--- Complete_Example/src/Orchestra.ts
+++ Complete_Example/src/Orchestra.ts
@@ -1,46 +1,66 @@
-import {GuitarPlayer} from "./guitarPlayer";
-import {catchError, interval, map, merge, Observable, share, Subject} from "rxjs";
-
+import {
+  catchError,
+  interval,
+  map,
+  merge,
+  Observable,
+  shareReplay,
+  Subject,
+  switchMap,
+  takeUntil,
+} from "rxjs";
+import { GuitarPlayer } from "./guitarPlayer";
 
 export class Orchestra {
+  private playerOne = new GuitarPlayer();
+  private playerTwo = new GuitarPlayer();
 
-    private playerOne = new GuitarPlayer();
-    private playerTwo = new GuitarPlayer();
+  private music: Observable<string>;
+  private metronoom: Observable<number>;
 
-    private music: Observable<string>;
-    private metronoom: Observable<number>;
+  private playSubject = new Subject<void>();
+  private stopSubject = new Subject<void>();
 
-    private playSubject = new Subject<void>();
-    private stopSubject = new Subject<void>();
-
-    constructor() {
-        this.metronoom = interval(500).pipe(share());
-        // @RUBEN why is this needed
-        this.metronoom.subscribe(a => console.log(a));
-        this.music = this.buildMusic();
-    }
-
-    buildMusic(): Observable<string> {
-        return merge(
-            this.playerOne.listenToMePlay(this.metronoom).pipe(map(n => "PLAYER ONE: " + n)),
-            this.playerTwo.listenToMePlay(this.metronoom).pipe(map(n => "PLAYER TWO: " + n)),
+  constructor() {
+    this.metronoom = this.playSubject.pipe(
+      switchMap(() =>
+        interval(500).pipe(
+          //take the interval until the stopSubject emits a value
+          //you need this on the inner observable not the outer because than you can no
+          //longer start the playSubject again since it's "completed"
+          takeUntil(this.stopSubject)
         )
-            .pipe(
-                catchError((err, caught) => caught)
-            )
-    }
+      ),
+      //share and replay the last value of the switchmapped playsubject
+      //this prevents the metronoom from restarting when an error is caught and retried
+      shareReplay(1)
+    );
+    // this.metronoom =     interval(500).pipe(share());
+    // @RUBEN why is this needed
+    // this.metronoom.subscribe((a) => console.log(a));
+    this.music = this.buildMusic();
+  }
 
+  buildMusic(): Observable<string> {
+    return merge(
+      this.playerOne
+        .listenToMePlay(this.metronoom)
+        .pipe(map((n) => "PLAYER ONE: " + n)),
+      this.playerTwo
+        .listenToMePlay(this.metronoom)
+        .pipe(map((n) => "PLAYER TWO: " + n))
+    ).pipe(catchError((err, caught) => caught));
+  }
 
-    getMusic() {
-        return this.music
-    }
+  getMusic() {
+    return this.music;
+  }
 
-    stop() {
-        this.stopSubject.next();
-    }
+  stop() {
+    this.stopSubject.next();
+  }
 
-    play() {
-        this.playSubject.next();
-    }
-
+  play() {
+    this.playSubject.next();
+  }
 }
--- Complete_Example/src/guitarPlayer.ts
+++ Complete_Example/src/guitarPlayer.ts
@@ -1,28 +1,29 @@
-import {concat, from, map, Observable, repeat, take, zip} from "rxjs";
-
+import { concat, from, map, Observable, repeat, take, zip } from "rxjs";
 
 export class GuitarPlayer {
-    private play: Observable<string>;
+  private play: Observable<string>;
 
-    constructor() {
-        const songOne = ["G", "A", "B", "G"];
-        const songTwo = ["B", "C", "D"];
-        const playTwoSong$ = concat(this.playSong(songOne), this.playSong(songTwo));
-        this.play = playTwoSong$;
-    }
+  constructor() {
+    const songOne = ["G", "A", "B", "G"];
+    const songTwo = ["B", "C", "D"];
+    const playTwoSong$ = concat(this.playSong(songOne), this.playSong(songTwo));
+    this.play = playTwoSong$;
+  }
 
-    public playSong(notes: string[]) {
-        return from(notes);
-    }
+  public playSong(notes: string[]) {
+    return from(notes);
+  }
 
-    public listenToMePlay(metronoom: Observable<number>): Observable<string> {
-        return zip(metronoom, this.play.pipe(repeat(), take(1000)))
-            .pipe(map(part => part[1] + " " + Math.floor(part[0] / 7)),
-                map(t => {
-                    if (Math.random() > 0.8) {
-                        throw new Error("helahola");
-                    }
-                    return t;
-                }));
-    }
+  public listenToMePlay(metronoom: Observable<number>): Observable<string> {
+    return zip(metronoom, this.play.pipe(repeat(), take(1000))).pipe(
+      map((part) => part[1] + " " + Math.floor(part[0] / 7)),
+      map((t) => {
+        if (Math.random() > 0.8) {
+          console.log("throwing errror");
+          throw new Error("helahola");
+        }
+        return t;
+      })
+    );
+  }
 }
